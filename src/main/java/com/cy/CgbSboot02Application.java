11187d377e2a44a88adfac755b6fbe1191bd3fa5
1065747479@qq.com <zq263199>
2020-04-29 00:57:40 +0800

    完善客房信息

--- src/main/java/com/cy/CgbSboot02Application.java
+++ src/main/java/com/cy/CgbSboot02Application.java
@@ -1,9 +1,6 @@
 package com.cy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 @SpringBootApplication
 public class CgbSboot02Application {
 	static public void main(String[] args) {
--- src/main/java/com/cy/pj/sys/entity/Request.java
+++ src/main/java/com/cy/pj/sys/entity/Request.java
@@ -7,10 +7,4 @@ public class Request {
     private String username;
     private String password;
     private String source;
-//    path: %2Ffangyuan%2F
-//    domain: anjuke.com
-//    finger2: zh-TW|24|1.25|12|1536_864|1536_824|-480|1|1|1|undefined|1|unknown|Win32|unknown|3|false|false|false|false|false|0_false_false|d41d8cd98f00b204e9800998ecf8427e|f7bc91acfdafdfa98b7636d86db4a2f4
-//    psdk-d: jsdk
-//    psdk-v: 1.0.1
-//    callback: SDK_CALLBACK_FUN.successFun
 }
